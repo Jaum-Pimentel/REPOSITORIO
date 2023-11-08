947f8b13ea5889902781ed6c8c5fb596359514a9
Jaum-Pimentel <145376721+Jaum-Pimentel@users.noreply.github.com>
2023-11-08 17:15:13 -0300

    INICIAL commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrarResumos() {
+    const resumosContainer = document.getElementById('resumosContainer');
+    resumosContainer.style.display = 'flex';
+  
+    const header = document.querySelector('.header');
+    header.style.display = 'none';
+  }
+  function iniciar() {
+    const formulario = document.getElementById('formulario');
+    formulario.style.display = 'flex';
+  
+    const header = document.querySelector('.header');
+    header.style.display = 'none';
+  }
